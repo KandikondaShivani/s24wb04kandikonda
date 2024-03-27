71f2bedccb87e690fc9b53aaf55ca5623ffcc5b9
KandikondaShivani <156292076+KandikondaShivani@users.noreply.github.com>
2024-03-27 17:25:53 -0500

    Pushing code of Assignment 10 - Part 9 Adding Images

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dreamdestinationRouter = require('./routes/dreamdestination');
 var gridRouter = require('./routes/grid');
+var randomitemRouter = require('./routes/pick');
 
 
 var app = express();
@@ -26,6 +27,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/dreamdestination', dreamdestinationRouter);
 app.use('/grid', gridRouter);
+app.use('/selector', randomitemRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
